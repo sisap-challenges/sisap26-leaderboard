b6acb257dc6f60c81a05df41c84b0225372c9e2b
Maik Fröbe <maik.froebe@uni-jena.de>
2026-07-07 10:43:39 +0200

    prepare removal of dependencies to files.webis.de

---
+++ data/download-csv-files.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+wget https://files.webis.de/data-in-progress/data-research/sisap-2025/task-1-wikipedia-20260614-test.csv
+wget https://files.webis.de/data-in-progress/data-research/sisap-2025/task-1-wikipedia-small.csv
+wget https://files.webis.de/data-in-progress/data-research/sisap-2025/task-2-llama.csv
+wget https://files.webis.de/data-in-progress/data-research/sisap-2025/task-2-llama-pg174.csv
+wget https://files.webis.de/data-in-progress/data-research/sisap-2025/task-2-llama-dev.csv
+wget https://files.webis.de/data-in-progress/data-research/sisap-2025/task-3-nq-20260610-test.csv
+wget https://files.webis.de/data-in-progress/data-research/sisap-2025/task-3-fiqa.csv
